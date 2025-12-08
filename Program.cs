ba96ac51b2b4fdb9856cea5465ffb23ec2706927
Raul <raul@webonly>
2025-12-08 12:28:15 +0400

    minor

--- Program.cs
+++ Program.cs
@@ -7,8 +7,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins")
+    .Get<string[]>();
+
+
+if (allowedOrigins == null || allowedOrigins.Length == 0)
+{
+    throw new Exception("No allowed origins configured for CORS.");
+}
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OnlyAllowed", policy =>
+    {
+        policy.WithOrigins(allowedOrigins)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
+
 var app = builder.Build();
 
+
+app.UseCors("OnlyAllowed");
 app.UseOcelot().Wait();
 
 app.Run();
